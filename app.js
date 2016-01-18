fade157989bd41535c09905880e06e949730bbf5
Andrew MacMurray <a.macmurray@icloud.com>
2016-01-18 20:48:57 +0000

    fixed submit from refreshing page, added class on click to new component

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-window.addEventListener('keypress', function(e) {
+document.addEventListener('keypress', function(e) {
   if (e.keyCode === 13) {
+    e.preventDefault();
     var newTask = document.getElementById('input-new-task').value;
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode(newTask);
-    node.appendChild(textnode);
-    document.getElementById('tasks').appendChild(node);
-    document.getElementById('input-new-task').value = '';
+    console.log(document.getElementById('input-new-task'));
+    if (newTask !== '') {
+       var node = document.createElement("li")
+       node.className += 'task';
+       var textnode = document.createTextNode(newTask);
+       node.appendChild(textnode);
+       var finalNode = document.getElementById('tasks').appendChild(node);
+      finalNode.addEventListener('click', clickHandler)
+      document.getElementById('input-new-task').value = '';
+    }
   }
 }, false);
 
 document.getElementById('submit-button').addEventListener('click', function (e) {
   var newTask = document.getElementById('input-new-task').value;
-  var node = document.createElement("LI");
-  var textnode = document.createTextNode(newTask);
-  node.appendChild(textnode);
-  document.getElementById('tasks').appendChild(node);
-  document.getElementById('input-new-task').value = '';
-});
+  if (newTask !== '') {
+     var node = document.createElement("li");
+     var textnode = document.createTextNode(newTask);
+     node.appendChild(textnode);
+     document.getElementById('tasks').appendChild(node);
+     document.getElementById('input-new-task').value = '';
+  }
+}, false);
 
 
 
