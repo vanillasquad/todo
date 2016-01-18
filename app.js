251a0368f0e314b53e2e274d91e10a6dc76250ba
Katherine Bowler <katherine.bowler@gmail.com>
2016-01-18 16:58:34 +0000

    Change submit to button & add functionality for submit

--- app.js
+++ app.js
@@ -9,9 +9,28 @@
 
 */
 
-document.getElementById('submit-button').onclick = function (e) {
-  // takes info in input box and adds it to task list
-  var newTask = document.getElementById('input-new-task');
-  // document.getElementById('input-new-task').value = ""; should clear input box
-  document.getElementById('tasks').appendChild(newTask); // should add task to list
-};
+// takes info in input box and adds it to task list
+window.addEventListener('keypress', function(e) {
+  if (e.keyCode === 13) {
+    var newTask = document.getElementById('input-new-task').value;
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode(newTask);
+    node.appendChild(textnode);
+    document.getElementById('tasks').appendChild(node);
+    document.getElementById('input-new-task').value = '';
+  }
+}, false);
+
+document.getElementById('submit-button').addEventListener('click', function (e) {
+  var newTask = document.getElementById('input-new-task').value;
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode(newTask);
+  node.appendChild(textnode);
+  document.getElementById('tasks').appendChild(node);
+  document.getElementById('input-new-task').value = '';
+});
+
+
+
+
+// document.getElementById('tasks').appendChild(newTask); // should add task to list
