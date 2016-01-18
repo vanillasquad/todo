67487dfe7085f485f35cfe61eb0535b598431fed
Andrew MacMurray <a.macmurray@icloud.com>
2016-01-18 20:57:06 +0000

    starting to add clear function

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+// Task-click handler
+var clickHandler = function(e) {
+   this.classList.toggle('completed');
+};
+
 document.addEventListener('keypress', function(e) {
   if (e.keyCode === 13) {
     e.preventDefault();
     var newTask = document.getElementById('input-new-task').value;
-    console.log(document.getElementById('input-new-task'));
     if (newTask !== '') {
        var node = document.createElement("li")
        node.className += 'task';
        var textnode = document.createTextNode(newTask);
        node.appendChild(textnode);
        var finalNode = document.getElementById('tasks').appendChild(node);
-      finalNode.addEventListener('click', clickHandler)
+       finalNode.addEventListener('click', clickHandler)
       document.getElementById('input-new-task').value = '';
     }
   }
@@ -19,21 +23,16 @@ document.getElementById('submit-button').addEventListener('click', function (e) 
   var newTask = document.getElementById('input-new-task').value;
   if (newTask !== '') {
      var node = document.createElement("li");
+     node.className += 'task';
      var textnode = document.createTextNode(newTask);
      node.appendChild(textnode);
-     document.getElementById('tasks').appendChild(node);
+     var finalNode = document.getElementById('tasks').appendChild(node);
+     finalNode.addEventListener('click', clickHandler)
      document.getElementById('input-new-task').value = '';
   }
 }, false);
 
-
-
-// Task-click handler
-var clickHandler = function(e) {
-    this.classList.toggle('completed');
-};
-
-var tasks = document.querySelectorAll('ul#tasks li');
-for (var ii=0, len=tasks.length; ii<len; ii++) {
-    tasks[ii].addEventListener('click', clickHandler);
-}
+document.getElementById('clear-completed').addEventListener('click', function(e) {
+   var clearItems = document.querySelectorAll('completed');
+   console.log(clearItems);
+})
