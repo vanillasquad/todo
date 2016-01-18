0422f184b815168f93d8aa09ca80f4630aaad02e
Andrew <a.macmurray@icloud.com>
2016-01-18 16:58:08 +0000

    made it look prettyish

--- app.js
+++ app.js
@@ -8,13 +8,13 @@
 });
 
 */
-
-document.getElementById('submit-button').onclick = function (e) {
-  // takes info in input box and adds it to task list
-  var newTask = document.getElementById('input-new-task');
-  // document.getElementById('input-new-task').value = ""; should clear input box
-  document.getElementById('tasks').appendChild(newTask); // should add task to list
-};
+// 
+// document.getElementById('submit-button').onclick = function (e) {
+//   // takes info in input box and adds it to task list
+//   var newTask = document.getElementById('input-new-task');
+//   // document.getElementById('input-new-task').value = ""; should clear input box
+//   document.getElementById('tasks').appendChild(newTask); // should add task to list
+// };
 
 
 
