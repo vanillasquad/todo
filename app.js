391ae844b1464eef7928f950d2022ae8f93a09ad
spudtrader <jmurphy.web@gmail.com>
2016-01-18 16:01:29 +0000

    fixed submit button click functionality

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 
-
-document.querySelector('form.task-list input[type=button]').addEventListener('click', function(e) {
+/*document.querySelector('form.task-list input[type=button]').addEventListener('click', function(e) {
   // takes info in input box and adds it to task list
   var newTask = document.getElementById('input-new-task');
   // document.getElementById('input-new-task').value = ""; should clear input box
   document.getElementById('tasks').appendChild(newTask); // should add task to list
-  
+
 });
+
+*/
+
+document.getElementById('submit-button').onclick = function (e) {
+  // takes info in input box and adds it to task list
+  var newTask = document.getElementById('input-new-task');
+  // document.getElementById('input-new-task').value = ""; should clear input box
+  document.getElementById('tasks').appendChild(newTask); // should add task to list
+};
