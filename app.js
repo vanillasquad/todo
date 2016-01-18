df65ea3d5b9e5d64945e3427e1c86350053c8f89
Andrew MacMurray <a.macmurray@icloud.com>
2016-01-18 21:49:17 +0000

    changed to on submit

--- app.js
+++ app.js
@@ -3,25 +3,9 @@ var clickHandler = function(e) {
    this.classList.toggle('completed');
 };
 
-// add todo item when pressing return key
-document.addEventListener('keypress', function(e) {
-  if (e.keyCode === 13) {
-    e.preventDefault();
-    var newTask = document.getElementById('input-new-task').value;
-    if (newTask !== '') {
-       var node = document.createElement("li")
-       node.className += 'task';
-       var textnode = document.createTextNode(newTask);
-       node.appendChild(textnode);
-       var finalNode = document.getElementById('tasks').appendChild(node);
-       finalNode.addEventListener('click', clickHandler)
-      document.getElementById('input-new-task').value = '';
-    }
-  }
-}, false);
-
-// add todo item on button click
-document.getElementById('submit-button').addEventListener('click', function (e) {
+// add todo item on submit
+document.addEventListener('submit', function(e) {
+  e.preventDefault();
   var newTask = document.getElementById('input-new-task').value;
   if (newTask !== '') {
      var node = document.createElement("li");
